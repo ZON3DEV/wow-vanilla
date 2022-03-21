f0240a0ddfa10129a7f31f4479a34a0471173ba0
ZON3DEV <86509465+ZON3DEV@users.noreply.github.com>
2022-03-21 20:32:37 +0100

    Create Get_Gold_on_Start.lua

---
+++ LuaScript/lua_scripts/custom/Get_Gold_on_Start.lua
@@ -0,0 +1,19 @@
+function OnFirstEnterWorld(event, player)
+player:AddCoinage(10000000)
+end
+end
+
+RegisterServerHook (3, "OnFirstEnterWorld")
+
+
+Currently: 10k gold reward
+
+1 - 1copper
+10 - 10 copper
+100 - 1 silver
+1000 - 10 silver
+10000 - 1 gold
+100000 - 10 gold
+1000000 - 100 gold
+10000000 - 1000 gold
+100000000 - 10000 gold
